f80b6e0ecbb4e1585e9d1a5aef6fd66aae086d8a
danish <er_danish@ymail.com>
2020-07-29 15:27:08 +0530

    aa

--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -1,6 +1,15 @@
 console.log("app is running");
 
-var template = <div><p>indecision app</p><p>some info</p></div> ;
+var template = (
+    <div>
+        <h1>indecision app</h1>
+        <p>some info</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div> 
+);
 
 var appRoot= document.getElementById('app');
 
